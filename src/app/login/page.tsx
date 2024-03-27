4c6fd286e3429e29d327f5a4b166f7d32111fced
Daniel Franca <daniel.pe.15y15@gmail.com>
2024-03-27 19:34:55 -0300

    fist commit

---
+++ src/app/login/page.tsx
@@ -0,0 +1,15 @@
+
+import Baner from "../components/baner"
+export default function Login() {
+    return (
+        <div className="flex min-h-screen flex-col items-center justify-center p-24">
+            <h1 className="text-5xl">Login session</h1>
+            <a className="mt-5" href="/">
+                <button className="btn btn-secondary">
+                    home
+                </button>
+            </a>
+            <Baner/>
+        </div>
+    )
+}
